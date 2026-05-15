358d71bd5a0899b5c9879b1e51c2c024971d3862
Aleix3 <Aleixbotella.1@gmail.com>
2026-05-15 12:24:02 +0200

    ols projectile idle animation

--- Suregs_Masks_Remake/Assets/Scripts/Enemies/EnemyOls.cs
+++ Suregs_Masks_Remake/Assets/Scripts/Enemies/EnemyOls.cs
@@ -57,19 +57,37 @@ public class EnemyOls : Enemy
     {
         canMove = false;
         isNotAttacking = false;
+
         Vector2 flipDirection = (player.position - transform.position).normalized;
+
         if (flipDirection.x > 0 && isFacingLeft) Flip();
         else if (flipDirection.x < 0 && !isFacingLeft) Flip();
+
         animator.SetTrigger("Attack");
+
         yield return new WaitForSeconds(0.8f);
-        GameObject newProjectile = Instantiate(olsProjectile, transform.position, Quaternion.identity);
+
+        Vector2 direction = (player.position - transform.position).normalized;
+
+        GameObject newProjectile = Instantiate(
+            olsProjectile,
+            transform.position,
+            Quaternion.identity
+        );
+
+        // ROTAR EL PROYECTIL HACIA DONDE VA
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        newProjectile.transform.rotation = Quaternion.Euler(0, 0, angle + 180f);
+
         Ols_Projectile newOlsProjectile = newProjectile.GetComponent<Ols_Projectile>();
         newOlsProjectile.damage = attackDamage;
-        Vector2 direction = (player.position - transform.position).normalized;
+
         newProjectile.GetComponent<Rigidbody2D>().velocity = direction * projectileSpeed;
+
         canMove = true;
-        
+
         yield return new WaitForSeconds(attackCooldown);
+
         isNotAttacking = true;
     }
 
