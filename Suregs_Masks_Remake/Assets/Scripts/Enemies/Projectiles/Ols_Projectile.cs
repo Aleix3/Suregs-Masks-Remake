6aad47133ab0a9e5ab98c3aa65a616947a62100d
Aleix3 <Aleixbotella.1@gmail.com>
2026-08-10 23:53:02 +0200

    ols projectiles destroyed on walls

--- Suregs_Masks_Remake/Assets/Scripts/Enemies/Projectiles/Ols_Projectile.cs
+++ Suregs_Masks_Remake/Assets/Scripts/Enemies/Projectiles/Ols_Projectile.cs
@@ -18,6 +18,10 @@ public class Ols_Projectile : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.CompareTag("Wall"))
+        {
+                Destroy(this.gameObject);
+        }
         if (collision.CompareTag("Player"))
         {
             Player player = collision.GetComponent<Player>();
